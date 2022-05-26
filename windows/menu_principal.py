ae05b3e06eb23f7deee4a09e7c6f7df82b57ce89
Isidro Emir Pelli <isidroemirpelli@gmail.com>
2022-05-25 21:11:35 -0300

    Creacion de interfaz de profile y reparacion del menu al acceder a profile.

--- windows/menu_principal.py
+++ windows/menu_principal.py
@@ -1,4 +1,5 @@
 import PySimpleGUI as sg
+import os
 import profile_screen, config_screen, game_screen, scores_screen
 
 
@@ -15,11 +16,11 @@ def build():
 
     layout = [
         [sg.Push(), sg.Column(col, element_justification="right")],
-        [sg.Push(), sg.Image(filename="logo.png"), sg.Push()],
+        [sg.Push(), sg.Image(filename=os.path.join(os.getcwd(), 'assets', 'logo.png')), sg.Push()],
         [sg.Push(), sg.Button("Jugar", font="Verdana 12", border_width=2, size=(13,1), key="-GAME-"), sg.Push()],
         [sg.Push(), sg.Button("Configuración", font="Verdana 12", border_width=2, size=(13,1), key="-CONFIG-"), sg.Push()],
         [sg.Push(), sg.Button("Puntajes", font="Verdana 12", border_width=2, size=(13,1), key="-SCORES-"), sg.Push()],
-        [sg.Push(), sg.Button("Perfiles", font="Verdana 12", border_width=2, size=(13,1), key="-PROFILE"), sg.Push()],
+        [sg.Push(), sg.Button("Perfiles", font="Verdana 12", border_width=2, size=(13,1), key="-PROFILE-"), sg.Push()],
     ]
 
     window = sg.Window("FiguRace", layout, resizable=True, size=(500, 600), auto_size_buttons=True,
