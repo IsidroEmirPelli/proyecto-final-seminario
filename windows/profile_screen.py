0c0ac7486358cf2da706cd02bd4f35ddf44be7d3
IsidroEmirPelli <isidroemirpelli@gmail.com>
2022-06-01 00:02:03 -0300

    Docstring y comentarios reparados

--- windows/profile_screen.py
+++ windows/profile_screen.py
@@ -10,35 +10,36 @@ def creating_user_file(values):
     """Genero un diccionario con los valores y lo pongo dentro de una lista"""
     user_dict = [{"Nickname": values['-NICK-'], "Edad": values['-EDAD-'], "Genero": values['-GENERO-']}]
     with open(getting_path(), 'w') as users:
-        """Agrego la lista al json"""
+        #Agrego la lista al json
         json.dump(user_dict, users, indent=4)
+
 
 def create_new_user(values):
     """Recibo los valores del usuario nuevo y lo almaceno en un diccionario"""
     new_user = {"Nickname": values['-NICK-'], "Edad": values['-EDAD-'], "Genero": values['-GENERO-']}
     with open(getting_path(), 'r') as users:
-        """Cargo el json"""
+        #Cargo el json
         users_list = json.load(users)
-    """Y lo agrego a la lista de dicciorios que hay en el json"""
+    #Y lo agrego a la lista de dicciorios que hay en el json
     users_list.append(new_user)
     with open(getting_path(), 'w') as users:
-        """Actualizo el contenido del json"""
+        #Actualizo el contenido del json
         json.dump(users_list, users, indent=4)
 
 
 def modify_user(values):
     with open(getting_path(), 'r') as users:
-        """Abro el json"""
+        #Abro el json
         users_list = json.load(users)
     for elem in users_list:
-        """Obtengo la ubicaion de la lista del usuario que deseo modificar"""
+        #Obtengo la ubicaion de la lista del usuario que deseo modificar
         if elem['Nickname'] == values['-NICK-']:
-            """Una vez que lo encuentro reemplazo el contenido con el nuevo"""
+            #Una vez que lo encuentro reemplazo el contenido con el nuevo
             elem['Edad'] = values['-EDAD-']
             elem['Genero'] = values['-GENERO-']
             break
     with open(getting_path(), 'w') as users:
-        """Actualizo el contenido del json con la lista modificada"""
+        #Actualizo el contenido del json con la lista modificada
         json.dump(users_list, users, indent=4)
 
 
@@ -52,17 +53,17 @@ def check_profile(values):
     except FileNotFoundError:
         creating_user_file(values)
     else:
-        """Verifico si existe el usuario si no se cumple la excepcion anterior"""
+        #Verifico si existe el usuario si no se cumple la excepcion anterior
         found = False
         for user in users:
             if values['-NICK-'] == user['Nickname']:
                 found = True
                 break
         if found:
-            """Si encuentro el usuario lo modifico"""
+            #Si encuentro el usuario lo modifico
             modify_user(values)
         else:
-            """Sino lo genero y lo inserto en el json"""
+            #Sino lo genero y lo inserto en el json
             create_new_user(values)
 
 
@@ -76,7 +77,7 @@ def build():
     padding_text = (40, 0)
     sg.theme("LightBlue")
 
-    """Creacion de LayOut"""
+    #Creacion de LayOut
     layout = [
         [sg.Text('Creacion/Edicion de perfil', pad=(41, 0), font=('Any 25'))],
         [sg.Text("Nickname", pad=padding_text), sg.Input(key='-NICK-', size=size_input)],
@@ -90,19 +91,19 @@ def build():
     window = sg.Window("FiguRace *-* Perfiles", layout, resizable=False, margins=(20, 20), size=(500, 200),
                        auto_size_buttons=True, keep_on_top=False, finalize=True)
     while True:
-        """Inicio la ventana"""
+        #Inicio la ventana
         event, values = window.read()
         if event == '-ACEPTAR-':
-            """Verifico que los campos no esten vacios."""
+            #Verifico que los campos no esten vacios.
             if (values['-NICK-'] == '' or values['-EDAD-'] == 0 or values['-GENERO-'] == ''):
                 sg.popup('Por favor complete todos los campos', title='Error')
             else:
-                """Verifico que no sea el valor predeterminado"""
+                #Verifico que no sea el valor predeterminado
                 if (values['-NICK-'] == 'Usuarios'):
                     sg.popup('Nickname invalido', title='Error')
                     window['-NICK-'].update('')
                 else:
-                    """Verifico si el genero es valido ya que se puedes escribir en esa casilla"""
+                    #Verifico si el genero es valido ya que se puedes escribir en esa casilla
                     if values['-GENERO-'] in generos:
                         check_profile(values)
                         sg.popup('Perfil creado/modificado con exito')
@@ -110,7 +111,7 @@ def build():
                         sg.popup('Por favor seleccione un genero valido', title='Error')
                         window['-GENERO-'].update('')
         elif event == '-BORRAR-':
-            """Actualizo el contenido de todos los espacios para que no contengan nada."""
+            #Actualizo el contenido de todos los espacios para que no contengan nada.
             window['-NICK-'].update('')
             window['-EDAD-'].update(0)
             window['-GENERO-'].update('')
