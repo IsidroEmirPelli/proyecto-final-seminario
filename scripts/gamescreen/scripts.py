9293af0997ff876b4549e6d448e3d7851309c147
Santiago Andrés, Cecconato <santycheco1@gmail.com>
2022-06-19 18:39:46 -0300

    integracion timer

--- scripts/gamescreen/scripts.py
+++ scripts/gamescreen/scripts.py
@@ -3,6 +3,7 @@ import csv
 from random import choice, randrange
 import PySimpleGUI as sg
 from scripts.gamescreen.constant import *
+from time import time
 
 
 def get_config():
@@ -38,7 +39,7 @@ def create_col_result(clave_al, user):
 
     config = get_config()
     check = [[sg.Text(f'{index}- ', size=(4, 1)), sg.Image('', size=(15, 15), key=f'-IMG_{index}-')] for index in
-             range(1, (config['Rondas']+1))]
+             range(1, (config['Rondas'] + 1))]
     col_resultado_parcial = [
         [sg.Text(f"{user}")],
         [sg.Column(check)],
@@ -59,9 +60,10 @@ def otras_cartas(num_car, data) -> int:
     """retorna "cartas" aleatoria que no sea la que se esta jugando"""
 
     act_cards = []
-    while len(act_cards) != 4:
+    while len(act_cards) < 4:
         num = randrange(len(data))
-        if num not in act_cards and num != num_car:
+        # 3 -> para que no repita el nombre del artista
+        if num not in act_cards and num != num_car and data[num][5] != data[num_car][5]:
             act_cards.append(num)
 
     return act_cards
@@ -111,7 +113,7 @@ def create_right_col(header, data, dificultad, num_carta):
         [sg.Text(f'Nivel: {dificultad}')],  # -> determinar la dificultad
         [sg.Text(f'Tiempo total: {10}min', justification="right", font=GEN_FONT)],
         # -> determinar el tiempo segun la dificultad o configuracion
-        [sg.Push(), sg.Text("00:00", justification="center", key="-Timer-", text_color='#ff0055', font=("Verdana", 12)),
+        [sg.Push(), sg.Text("00:00", justification="center", key="-TIMER-", text_color='#ff0055', font=("Verdana", 12)),
          sg.Push()],
         [sg.Frame(f'Tarjeta {"1"}', [[sg.Column(layout=box_tarjeta)]], font=GEN_FONT)],
     ]
@@ -129,6 +131,7 @@ def window_update(window, dificultad):
     num_carta = randrange(len(data))  # obtengo carta a jugar aleatoriamente
     otras_cards = otras_cartas(num_carta, data)
 
+    # retorna lista con el dato a encontrar
     cartas = [f'{data[otras_cards[index]][5]}' for index in range(len(otras_cards))]
     cartas.append(f'{data[num_carta][5]}')
 
@@ -143,3 +146,23 @@ def window_update(window, dificultad):
         window[f'-CARTA_{index}-'].update(f'{cartas[index - 1]}')
 
     return num_carta
+
+
+def countdown(window, start_time, config, data, dificultad, img_act):
+    """Controlador del countdown"""
+    current_time = int(time() - start_time)
+    elapsed_time = config['Tiempo'] - current_time  # tiempo restante = inicial -transcurrido
+    if elapsed_time > 10:
+        window['-TIMER-'].update(value=f' {elapsed_time // 60}:{elapsed_time % 60}', font=('verdana', 13),
+                                 text_color='#35F64F')
+    elif elapsed_time >= 0:
+        # -> solo cambio de color
+        window['-TIMER-'].update(value=f' {elapsed_time // 60}:{elapsed_time % 60}', font=('verdana', 13),
+                                 text_color='#ff0055')
+    else:
+        # terminar ventana
+        sg.popup('Tiempo terminado', title='Game Over')
+        window[f'-IMG_{len(img_act) + 1}-'].update(PATH_NOTCHECK_PNG)
+        img_act.append(False)
+        carta_buena = data[window_update(window, dificultad)][5]
+        start_time = time()
--- windows/game_screen.py
+++ windows/game_screen.py
@@ -3,6 +3,7 @@ import PySimpleGUI as sg
 from random import randrange
 from scripts.gamescreen.scripts import *
 from scripts.gamescreen.constant import *
+from time import time
 
 
 def build(user, dificultad):
@@ -11,10 +12,10 @@ def build(user, dificultad):
     config = get_config()
     csv_selected, header, data = get_card_data()
     num_carta = randrange(len(data))  # obtengo carta a jugar aleatoriamente
-    
+
     # Construccion de la ventana del juego
     sg.theme("LightBlue")
-    
+
     # Columna izquierda resultado parcial
     col_left = create_col_result(csv_selected, user)
     col_right = create_right_col(header, data, dificultad, num_carta)
@@ -28,22 +29,31 @@ def build(user, dificultad):
     window = sg.Window("FiguRace *-* ¡A jugar!", layout, resizable=True, size=(700, 640), auto_size_buttons=True)
     img_act = []
     carta_buena = data[num_carta][5]
+    start_time = time()
+
     while True:
         if len(img_act) != config['Rondas']:
-            event, values = window.read()
+            event, values = window.read(timeout=250, timeout_key='-TIMEOUT-')
             if event == sg.WIN_CLOSED:
                 break
             elif event == '-VOLVER-':
                 window.close()
+
+            # countdown
+            elif event == '-TIMEOUT-':
+                countdown(window, start_time, config, data, dificultad, img_act)
             else:
                 if window[event].get_text() == carta_buena:
-                    window[f'-IMG_{len(img_act)+1}-'].update(PATH_CHECK_PNG)
+                    window[f'-IMG_{len(img_act) + 1}-'].update(PATH_CHECK_PNG)
                     img_act.append(True)
                     carta_buena = data[window_update(window, dificultad)][5]
-                else:
-                    window[f'-IMG_{len(img_act)+1}-'].update(PATH_NOTCHECK_PNG)
+                    start_time = time()
+                else:  # si llega aca carta perdida
+                    window[f'-IMG_{len(img_act) + 1}-'].update(PATH_NOTCHECK_PNG)
                     img_act.append(False)
                     carta_buena = data[window_update(window, dificultad)][5]
+                    start_time = time()
+
         else:
             sg.popup("No puedes seguir jugando")
             window.close()
